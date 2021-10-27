d600cd4e20425fb793457687b4ad427d8f5a8aa6
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-27 16:31:35 +0200

    finito il css e l'html

---
+++ my_script.js
@@ -0,0 +1,30 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+//devo creare dei div che contengano un img e un div con dentro il titolo e il testo per la parte grande 
+//nella parte piccola invece serve solo un div contenente un img
+//quando è presente il primo di un array devono essere presenti tutti gli altri due e se c'è ne sono infiniti dovranno sempre esserci
+
+
+//al click dell'utente nella freccia verso il basso
+    //l'immagine attiva diventa visibile a sinistra con i relativi titoli e testo
